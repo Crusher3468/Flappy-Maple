439bf9f7307a4a6f44e452a190b618239c796fbf
Crusher3468 <maxdragonrider@gmail.com>
2023-11-14 09:53:13 -0700

    stuff

--- Assets/Scripts/Points.cs
+++ Assets/Scripts/Points.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class Points : MonoBehaviour
 {
+    new int score = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        score++;
+        print(score);
     }
 }
